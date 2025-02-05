1cef1ab8f74e1d64f7f3db514186dbbcc3222a75
Kostya Lachin <prikolist04@mail.ru>
2025-02-05 10:22:37 +0300

    Reformate Code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.lang.module.FindException;
 public class Main {
     public static void main(String[] args) {
         BMiService bmiservice = new BMiService();
-        int BmiIndex = bmiservice.calculate (98, 1.87); // должно получиться 28
+        int BmiIndex = bmiservice.calculate(98, 1.87); // должно получиться 28
         System.out.println("Индекс массы тела (BMI):" + BmiIndex);
 
     }
